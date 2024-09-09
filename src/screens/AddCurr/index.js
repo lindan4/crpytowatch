47b749b310666f0d34fd919c0331bd8f2d08cffc
Lindan Thillanayagam <lindthilldev@gmail.com>
2024-09-08 20:32:43 -0400

    Modified styling of add curr page.

--- src/screens/AddCurr/index.js
+++ src/screens/AddCurr/index.js
@@ -3,9 +3,9 @@ import {
   View,
   Text,
   TextInput,
-  Button,
   StyleSheet,
   SafeAreaView,
+  TouchableOpacity,
 } from "react-native";
 import axios from "axios";
 import { useDispatch } from "react-redux";
@@ -27,20 +27,20 @@ const AddCurrScreen = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.viewStyle}>
-      <View>
-        <Text>Add a Cryptocurrency</Text>
+      <View style={styles.innerViewStyle}>
+        <Text style={styles.addCryptoTextStyle}>Add a Cryptocurrency</Text>
         <TextInput
+          style={styles.symbolTextInputStyle}
           placeholder="Enter name or ticket symbol"
           value={crypInput}
           onChangeText={(val) => setCrypInput(val)}
         />
-        <View>
-          <Button
-            title="Add"
-            disabled={crypInput.length == 0}
-            onPress={onAddPress}
-          />
+        <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
+          <TouchableOpacity style={styles.addPressStyle} onPress={onAddPress}>
+            <Text style={styles.addTextStyle}>Add</Text>
+          </TouchableOpacity>
         </View>
+
         {isError && <Text>The currency does not exist.</Text>}
       </View>
     </SafeAreaView>
@@ -52,6 +52,28 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
+  },
+  innerViewStyle: {
+    paddingHorizontal: 15,
+    rowGap: 10,
+  },
+  addCryptoTextStyle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  symbolTextInputStyle: {
+    borderWidth: 0.5,
+    borderRadius: 7,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+  },
+  addPressStyle: {
+    backgroundColor: "#f4d465",
+  },
+  addTextStyle: {
+    fontWeight: "bold",
+    paddingHorizontal: 25,
+    paddingVertical: 15,
   },
 });
 
